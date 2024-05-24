a303f5e4fc4ec210d8303629ae6d6a301328ad2d
fernadaArrieta <arrietafernanda6@gmail.com>
2024-05-24 16:04:23 -0300

    validacion de formulario completa por js

--- main.js
+++ main.js
@@ -4,51 +4,52 @@ function validar(event) {
   let edad = document.getElementById("edad").value;
   let email = document.getElementById("email").value;
   let mostrarErrores = document.getElementById("errores");
-  let errores = [];
 
-  if (nombre.length === "") {
-    errores.push("El campo nombre es obligatorio");
-  } else if (nombre.length > 35) {
-    errores.push("Se permite un maximo de 35 caracteres ");
+  mostrarErrores.innerHTML = "";
+  let esValido = true;
+
+  if (nombre === "" || nombre.length > 35) {
+    const spanError = document.createElement("span");
+    spanError.innerHTML = " *Ingrese un nombre válido";
+    spanError.className = "color-rojo";
+    mostrarErrores.appendChild(spanError);
+    mostrarErrores.appendChild(document.createElement("br"));
+    esValido = false;
   }
 
-  if (isNaN(edad) || edad < 0 || edad > 100) {
-    errores.push("La edad debe estar en un rango entre 0 y 100 años");
+  if (edad == "" || edad < 0 || edad > 100) {
+    const spanError = document.createElement("span");
+    spanError.innerHTML = " *La edad debe ser un número entre 1 y 100";
+    spanError.className = "color-rojo";
+    mostrarErrores.appendChild(spanError);
+    mostrarErrores.appendChild(document.createElement("br"));
+    esValido = false;
   }
   if (email === "") {
-    errores.push("Por favor introduce una dirección de correo electronico");
-  } else if (email.length > 0) {
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      errores.push("El correo electrónico ingresado no es válido.");
-    }
-  }
-  if (errores.length > 0) {
-    console.log(errores);
     const spanError = document.createElement("span");
-    spanError.textContent = errores.join(" ");
+    spanError.innerHTML =
+      " *Por favor introduce una dirección de correo electrónico";
     spanError.className = "color-rojo";
-    mostrarErrores.insertAdjacentElement("afterend", spanError);
-    console.log(spanError);
-
-    /* mostrarErrores(errores); */
-    //console.log(mostrarErrores(errores));
-    return false;
+    mostrarErrores.appendChild(spanError);
+    mostrarErrores.appendChild(document.createElement("br"));
+    esValido = false;
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    const spanError = document.createElement("span");
+    spanError.innerHTML = " *El correo electrónico ingresado no es válido";
+    spanError.className = "color-rojo";
+    mostrarErrores.appendChild(spanError);
+    mostrarErrores.appendChild(document.createElement("br"));
+    esValido = false;
   }
+
   mostrarMensaje(nombre, edad, email);
   limpiarFormulario();
   return false;
 }
-/* function mostrarErrores(errores) {
-  let erroresDiv = document.querySelector(".error");
-  if (!erroresDiv) {
-    erroresDiv = document.createElement("div");
-    erroresDiv.className = "error";
-    document.body.insertBefore(erroresDiv, document.querySelector("form"));
-  }
-  erroresDiv.innerHTML = errores.join("<br >");
-} */
+
 function mostrarMensaje(nombre, edad, email) {
   let mensajesUl = document.getElementById("mensajes");
+  mensajesUl.innerHTML = "";
   let mensajeLi = document.createElement("li");
   let mensaje;
 
